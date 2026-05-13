014b39a7608579fb90a4d8f679c1017a0252842d
Fla2001 <147964479+Fla2001@users.noreply.github.com>
2026-05-12 20:03:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ st.sidebar.title("Parámetros")
 st.write("Elaborado por: Flavia Valencia")
 
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
+
+if sesion == "Sesión 1":
+  st.write("Bienvenido a la sesión1")
+  st.image ("Python_logo.png")
+
